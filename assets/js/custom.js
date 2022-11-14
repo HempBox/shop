cea7b5d19b3fc7039b5cc8e9ca4ffca71614b66a
HempBox <116299071+HempBox@users.noreply.github.com>
2022-11-14 15:40:11 +0000

    a

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,19 +9,6 @@
 
     });
 
-	// WOW JS
-	$(window).on ('load', function (){
-        if ($(".wow").length) { 
-            var wow = new WOW ({
-                boxClass:     'wow',      // Animated element css class (default is wow)
-                animateClass: 'animated', // Animation css class (default is animated)
-                offset:       20,         // Distance to the element when triggering the animation (default is 0)
-                mobile:       true,       // Trigger animations on mobile devices (default is true)
-                live:         true,       // Act on asynchronously loaded content (default is true)
-            });
-            wow.init();
-        }
-    });
 
 	$(window).scroll(function() {
 	  var scroll = $(window).scrollTop();
@@ -93,14 +80,14 @@
 			  0:{
 				  items:1
 			  },
-			  600:{
+			  800:{
 				  items:2
 			  },
-			  1200:{
+			  1000:{
 				  items:3
 			  },
 			  1800:{
-				items:3
+				items:4
 			}
 		}
 	})
